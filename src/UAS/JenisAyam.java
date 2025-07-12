c0787c4a3b932ea4e32e894dae5f83a1928e193e
syarif <syarifridwan433@gmail.com>
2025-07-12 14:12:51 +0700

    update

--- src/UAS/JenisAyam.java
+++ src/UAS/JenisAyam.java
@@ -6,15 +6,18 @@ public class JenisAyam extends PeternakanAyam {
         super(Ras, JenisKelamin, umur);
     }
     
-    public void seleksiJenisAyam() {
-        // Seleksi berdasarkan Ras
+    //Polymorphism @ovverride
+    @Override
+    public void infoAyam() {
+        super.infoAyam();  
+        // seleksi
         switch (getRas()) {
             case "Petelur" -> {
-                System.out.println("Jenis Ayam: Petelur");
+                System.out.println("Ras Ayam: Petelur");
                 System.out.println("Ayam ini cocok untuk produksi telur.");
             }
             case "Pedaging" -> {
-                System.out.println("Jenis Ayam: Pedaging");
+                System.out.println("Ras Ayam: Pedaging");
                 System.out.println("Ayam ini cocok untuk produksi daging.");
             }
             default -> System.out.println("Jenis Ayam: Tidak dikenal");
--- src/UAS/PeternakanAyamBanjarmasin.java
+++ src/UAS/PeternakanAyamBanjarmasin.java
@@ -48,7 +48,6 @@ public class PeternakanAyamBanjarmasin {
         for (int i = 0; i < ayamArray.length; i++) {
             System.out.println("Ayam ke-" + (i + 1));
             ayamArray[i].infoAyam();
-            ayamArray[i].seleksiJenisAyam();
             System.out.println("==================="); 
         }
 
